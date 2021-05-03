f494a97cc224761d20d891ad059267d6ea7d7d3e
SomeStupidIdiot <james.yung.lee@gmail.com>
2021-05-02 22:59:25 -0400

    bookmark looks nicer

--- frontend/src/components/misc/Bookmark.tsx
+++ frontend/src/components/misc/Bookmark.tsx
@@ -10,79 +10,102 @@ import {
   ListItemSecondaryAction,
   Tooltip,
   Link,
+  Theme,
+  makeStyles,
+  Grid,
 } from "@material-ui/core";
 import Navbar from "../util/Navbar";
 import { getCollections, setClips } from "../../storage/storage";
 import DeleteIcon from "@material-ui/icons/Delete";
 import { getExternal } from "../../services/twitch";
 import React from "react";
-
+const useStyles = makeStyles((theme: Theme) => {
+  return {
+    container: {
+      marginTop: theme.spacing(2),
+    },
+    card: {
+      height: "100%",
+    },
+  };
+});
 const Bookmark = () => {
+  const classes = useStyles();
   const [collections, setCollections] = React.useState(getCollections());
   return (
     <>
       <Navbar />
-      <Container>
-        {(() => {
-          const cards: JSX.Element[] = [];
-          for (const id in collections) {
-            const curr = collections[id];
-            cards.push(
-              <Card key={id}>
-                <CardContent>
-                  <Typography variant="h6">
-                    <Link href={`/search/${id}`}>{curr.name}</Link>
-                  </Typography>
-                  <List>
-                    {curr.clips.map(
-                      ({ name, createdAt, startTime, endTime, quality }, i) => {
-                        const date = new Date(createdAt);
-                        return (
-                          <ListItem
-                            button
-                            onClick={() => {
-                              getExternal(
-                                `https://www.twitch.tv/videos/${id}?t=${startTime}s`
-                              );
-                            }}
-                          >
-                            <ListItemText
-                              primary={`${name} (${
-                                endTime - startTime
-                              }s long at ${quality})`}
-                              secondary={`Created at ${date.toLocaleTimeString()} on ${date.toLocaleDateString()}`}
-                            />
-                            <ListItemSecondaryAction>
-                              <Tooltip
-                                title="Delete this clip"
-                                aria-label="delete clip"
+      <Container className={classes.container} maxWidth={false}>
+        <Grid container spacing={2} alignItems="stretch">
+          {(() => {
+            const cards: JSX.Element[] = [];
+            for (const id in collections) {
+              const curr = collections[id];
+              cards.push(
+                <Grid item xs={12} md={6} lg={4} xl={3} key={id}>
+                  <Card className={classes.card}>
+                    <CardContent>
+                      <Typography variant="h6">
+                        <Link href={`/search/${id}`}>{curr.name}</Link>
+                      </Typography>
+                      <List>
+                        {curr.clips.map(
+                          (
+                            { name, createdAt, startTime, endTime, quality },
+                            i
+                          ) => {
+                            const date = new Date(createdAt);
+                            return (
+                              <ListItem
+                                button
+                                onClick={() => {
+                                  getExternal(
+                                    `https://www.twitch.tv/videos/${id}?t=${startTime}s`
+                                  );
+                                }}
+                                key={createdAt}
                               >
-                                <IconButton edge="end" aria-label="delete clip">
-                                  <DeleteIcon
-                                    onClick={() => {
-                                      const cpy = curr.clips.filter(
-                                        (_, index) => index !== i
-                                      );
-                                      setClips(id, cpy);
-                                      const cpy2 = { ...collections };
-                                      cpy2[id] = { ...curr, clips: cpy };
-                                      setCollections(cpy2);
-                                    }}
-                                  />
-                                </IconButton>
-                              </Tooltip>
-                            </ListItemSecondaryAction>
-                          </ListItem>
-                        );
-                      }
-                    )}
-                  </List>
-                </CardContent>
-              </Card>
-            );
-          }
-          return cards;
-        })()}
+                                <ListItemText
+                                  primary={`${name} (${
+                                    endTime - startTime
+                                  }s long at ${quality})`}
+                                  secondary={`Created at ${date.toLocaleTimeString()} on ${date.toLocaleDateString()}`}
+                                />
+                                <ListItemSecondaryAction>
+                                  <Tooltip
+                                    title="Delete this clip"
+                                    aria-label="delete clip"
+                                  >
+                                    <IconButton
+                                      edge="end"
+                                      aria-label="delete clip"
+                                      onClick={() => {
+                                        const cpy = curr.clips.filter(
+                                          (_, index) => index !== i
+                                        );
+                                        setClips(id, cpy);
+                                        const cpy2 = { ...collections };
+                                        cpy2[id] = { ...curr, clips: cpy };
+                                        setCollections(cpy2);
+                                      }}
+                                    >
+                                      <DeleteIcon />
+                                    </IconButton>
+                                  </Tooltip>
+                                </ListItemSecondaryAction>
+                              </ListItem>
+                            );
+                          }
+                        )}
+                      </List>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              );
+            }
+            return cards;
+          })()}
+        </Grid>
       </Container>
     </>
   );
